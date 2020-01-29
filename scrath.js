d7fffe1f9213d49692d2fe3e26faaed92e9adccc
melvasquires <melva.a.squires@gmail.com>
2020-01-29 15:39:59 -0500

    added practice work for future stuff

---
+++ scrath.js
@@ -0,0 +1,31 @@
+var goOutside;
+var weather = "bad";
+
+if(weather === "nice"){
+    goOutside = true;
+} else if (weather === "okay") {
+    goOutside = true;
+} else {
+    goOutside = false;
+}
+
+if (goOutside) {
+    console.log("we are going outside");
+} else {
+    console.log("stay inside");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
