dbe6312c72746d666f9bb8be9815f976c8ecac24
melvasquires <melva.a.squires@gmail.com>
2020-01-29 15:54:30 -0500

    taliking about them robots

---
+++ robots.js
@@ -0,0 +1,17 @@
+var detroyed;
+var weather = "bad";
+
+if(weather === "nice"){
+    detroyed = false;
+} else if (weather === "okay") {
+    destroyed = false;
+} else {
+    destroyed = true;
+}
+
+if (destroyed) {
+    console.log("cry in sadness");
+} else {
+    console.log("jump with glee");
+}
+
